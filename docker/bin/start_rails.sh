95dcf3928c5fe97d0b64d4a23bd69edf1eb1ec30
phongtv-1971 <truong.van.phong@sun-asterisk.com>
2021-08-19 18:01:29 +0700

    run container with production

--- docker/bin/start_rails.sh
+++ docker/bin/start_rails.sh
@@ -9,4 +9,4 @@ bundle exec rails db:prepare db:migrate
 
 bundle exec rails webpacker:compile
 
-bundle exec rails s -p 3000 -P /tmp/xukabot.pid -b '0.0.0.0'
+bundle exec rails s -p 3000 -P /tmp/xukabot.pid -b '0.0.0.0' -e production
