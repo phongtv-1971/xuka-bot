b50757fc1192053ddfe61d5630d64e9df48403eb
phongtv-1971 <truong.van.phong@sun-asterisk.com>
2021-08-19 18:08:54 +0700

    hotfix

--- docker/bin/start_rails.sh
+++ docker/bin/start_rails.sh
@@ -3,10 +3,10 @@
 
 yarn install --check-files
 
-bundle install -e production
+RAILS_ENV=production bundle install
 
-bundle exec rails db:prepare db:migrate -e production
+RAILS_ENV=production bundle exec rails db:prepare db:migrate
 
-bundle exec rails webpacker:compile -e production
+RAILS_ENV=production bundle exec rails webpacker:compile
 
 bundle exec rails s -p 3000 -P /tmp/xukabot.pid -b '0.0.0.0' -e production
