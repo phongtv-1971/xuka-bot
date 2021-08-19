6acb5bc5366826dc2c607d9d9cd38b40d89897ce
phongtv-1971 <truong.van.phong@sun-asterisk.com>
2021-08-19 18:06:51 +0700

    start production config bash

--- docker/bin/start_rails.sh
+++ docker/bin/start_rails.sh
@@ -3,10 +3,10 @@
 
 yarn install --check-files
 
-bundle install
+bundle install -e production
 
-bundle exec rails db:prepare db:migrate
+bundle exec rails db:prepare db:migrate -e production
 
-bundle exec rails webpacker:compile
+bundle exec rails webpacker:compile -e production
 
 bundle exec rails s -p 3000 -P /tmp/xukabot.pid -b '0.0.0.0' -e production
