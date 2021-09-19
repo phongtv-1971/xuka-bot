0cb375899a8a0f4b161be08113a9719b45f99eec
phongtv-1971 <truong.van.phong@sun-asterisk.com>
2021-09-19 21:16:44 +0700

    Create a bot

--- app/controllers/admins/bots_controller.rb
+++ app/controllers/admins/bots_controller.rb
@@ -1,7 +1,24 @@
 class Admins::BotsController < Admins::BaseController
   skip_before_action :load_bot, only: [:new, :create]
 
+  def new
+    render layout: "admins/dashboard"
+  end
+
+  def create
+    Bot.create! params_create_bot.merge(admin_id: current_admin.id)
+
+    redirect_to admins_root_path
+  rescue ActiveRecord::RecordInvalid => errors
+    @resource = errors.record
+    render :new, layout: "admins/dashboard"
+  end
+
   def show; end
 
-  def create; end
+  private
+
+  def params_create_bot
+    params.permit :bot_name, :room_id, :token
+  end
 end
